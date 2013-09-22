d84eb3c8a2d5144ef5dcca3ceb7ca19a1eb76455
Baptiste Lagarde <blagarde@appenbutlerhill.com>
2013-09-22 14:23:41 +1000

    CHANGE: UI improvements. Did some ugly bits, but fit for purpose.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.uix.textinput import TextInput
 from jnius import autoclass
 import csv
 import datetime
 import sys
+import os
+
+
+class BetterTextInput(TextInput):
+    def _keyboard_on_key_down(self, window, keycode, text, modifiers):
+        self.hook()
+        super(BetterTextInput, self)._keyboard_on_key_down(window, keycode, text, modifiers)
+
+    def hook(self):        
+        raise NotImplementedError
 
 
 class MainApp(App):
@@ -14,12 +25,15 @@ class MainApp(App):
         Clock.schedule_interval(self.update, 0.2)
         self.intro = self.root.ids['intro']
         self.setpath()
+        self.root.ids['path'].text = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'readings.csv')
+        self.root.ids['path'].hook = self.stop
+        self.root.ids['path'].bind(focus=self.setpath)
         self.progress = self.root.ids['progress']
         self.capture = False
         self.n = 0
 
     def start(self):
-        self.n = 0
+        self.root.ids['path'].focus = False
         now = str(datetime.datetime.utcnow())
         self.intro.text = "Started capture at %s\n\nFile: %s\n" % (now, self.path)
         with open(self.path, 'w') as f:
@@ -28,9 +42,12 @@ class MainApp(App):
         self.capture = True
 
     def stop(self):
+        if self.capture:
+            self.intro.text += "Ended capture at %s" % str(datetime.datetime.utcnow())
         self.capture = False
 
-    def setpath(self):
+    def setpath(self, *args):
+        self.n = 0
         self.path = self.root.ids['path'].text
         self.intro.text = "File path set to '%s'" % self.path
 
